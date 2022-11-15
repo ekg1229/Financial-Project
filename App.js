de578ef6f8f65f5da3342f837d959f6fcab04de0
n0eyh-os <ts7704@gmail.com>
2022-11-15 22:51:02 +0900

    종목 추천 웹페이지 디자인

---
+++ App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <nav>
+
+      </nav>
+      <div class="outline">
+        <center><h1>연관 주식 종목 추천</h1></center>
+        <div class="con1">
+          <br></br>
+          <center>중분류이름1</center>
+          <hr align="center" width="50%"></hr>
+          <p>&nbsp;&nbsp;1st: </p>
+          <p>&nbsp;&nbsp;2nd: </p>
+          <p>&nbsp;&nbsp;3nd: </p>
+        </div>
+	    </div>
+      <button name = "button" onclick = "location.href = '#'">나만의 포트폴리오<br/> 바로가기</button>
+      <footer>
+          @2022가천대학교_심화프로그래밍_고공비행
+      </footer>
+    </body>
+  );
+}
+
+export default App;
